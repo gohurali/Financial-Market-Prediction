843a5c2f1c51acaf4117cdcbc1a62ac317c35a95
gohurali <gohurali@live.com>
2019-07-04 00:08:03 -0700

    nn working?

--- train.py
+++ train.py
@@ -6,8 +6,11 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data
 import sklearn
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 """
 Basic multilayer perceptron subclass 
 of three layers in PyTorch
@@ -21,10 +24,19 @@ class MLP(nn.Module):
     
     def forward(self, x):
         """"Forward pass definition"""
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
         return x
+
+class TickerData(torch.utils.data.Dataset):
+    def __init__(self, table):
+        self.dataset = table
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self,idx):
+        """idx --> data pointer"""
+        return self.dataset[idx]
 
 def fetch_btc_prices():
     with open("BTC_data.json") as f:
@@ -44,22 +56,42 @@ def normalize():
 
 
 def data_split(x_train,y_train):
-        x_train, x_test, y_train, y_test = train_test_split(x_train,y_train,test_size=0.2,random_state=100)
+        x_train, x_test, y_train, y_test = train_test_split(x_train,y_train,test_size=0.2,random_state=100,shuffle=False)
         return x_train, x_test, y_train, y_test
 
-def train(model, x_train,y_train):
-    batch_size = 8
+def train(model, x_data,y_data):
 
-    x_train, x_test, y_train, y_test = data_split(x_train,y_train)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    min_max_scaler.fit_transform(y_data)
 
-    # Prep input data --> torch.float64
-    x_train = torch.from_numpy(x_train)
-    y_train = torch.from_numpy(y_train)
+    # x_train, x_test, y_train, y_test = data_split(x_train,y_train)
 
-    print(x_train.dtype)
-    print(y_train.dtype)
-    print(x_test.shape)
-    print(x_test.shape)
+    # # Prep input data --> torch.float64
+    # x_train = torch.from_numpy(x_train)
+    # y_train = torch.from_numpy(y_train)
+
+    # print(x_train.dtype)
+    # print(y_train.dtype)
+    # print(x_test.shape)
+    # print(x_test.shape)
+
+    print('----Dataset Prep----')
+    x_train, x_test, y_train, y_test = data_split(x_data,y_data)
+    train_tensorDataset = torch.utils.data.TensorDataset(torch.from_numpy(x_train),torch.from_numpy(y_train))
+    train_data_loader = torch.utils.data.DataLoader(
+        dataset=train_tensorDataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0
+    )
+
+    test_tensorDataset = torch.utils.data.TensorDataset(torch.Tensor(x_test),torch.Tensor(y_test))
+    test_data_loader = torch.utils.data.DataLoader(
+        dataset=test_tensorDataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0
+    )
 
     print('-- Model Architecture --')
     print(model)
@@ -69,22 +101,36 @@ def train(model, x_train,y_train):
 
     # -- Optimizer --> Adam generally works best
     # TODO: choose a better learning rate later
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-    for epoch in range(1000):
+    total_loss = 0
+    for epoch in (range(100)):
+        for i, (examples,labels) in tqdm(enumerate(train_data_loader)):
 
-        y_predictions = model(x_train.float())
-        loss = loss_func(y_predictions.float(),y_train.float())
+            #print(examples , " ----- " , labels)
+            
+            optimizer.zero_grad()
 
-        # zero out the gradients
-        optimizer.zero_grad()
-        # back-prop to update the weights
-        loss.backward()
-        # optimizer steps based on lr
-        optimizer.step()
+            y_predictions = model(examples.float())
+            loss = loss_func(y_predictions.float(),labels.float())
 
-        if epoch % 100 == 0:
-            print ('Epoch [%d/%d], Loss: %.4f' %(epoch+1, 100, loss.data[0].item()))
+            total_loss += loss.data
+
+            # back-prop to update the weights
+            loss.backward()
+            # optimizer steps based on lr
+            optimizer.step()
+
+            y_preds = y_predictions.detach().numpy()
+            print(y_preds.shape)
+            test = min_max_scaler.inverse_transform(y_preds)
+            print(test)
+
+            break
+
+        print ('Epoch [{}/{}], Loss: {}'.format(epoch+1, 100, loss.data))
+        #print(list(model.parameters()))
+        print("-----------------------------------------------------------------------------")
 
 
 def table_edit(dataframe):
@@ -97,11 +143,15 @@ def main():
     raw_price_data = fetch_btc_prices()
     data_df = parse_alphaV_JSON(raw_data=raw_price_data)
 
+    # -- Normalize the Data --
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data_df = pd.DataFrame(min_max_scaler.fit_transform(data_df), columns=data_df.columns)
+    # print(data_df)
+
     data_df = table_edit(data_df)
     y_train = np.array(data_df['4a. close (USD)'].tolist())
     data_df = data_df.drop(labels=['4a. close (USD)'],axis=1)
-    print(data_df)
-
+    
     model = MLP()
 
     train(model, data_df.values,y_train)
