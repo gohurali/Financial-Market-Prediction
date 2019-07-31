c52e7ba7408cbad7374269f5e93b0dd4347fba07
gohurali <gohurali@live.com>
2019-07-31 01:22:23 -0700

    updating scripts

--- inference.py
+++ inference.py
@@ -18,13 +18,46 @@ import urllib3
 import cryptocompare
 from datetime import datetime
 
-
 class Inferencer(object):
     def __init__(self):
         pass
+    def un_normalize(self,norm_val,min_val,max_val,typelist=None):
+        if(typelist):
+            for idx,item in enumerate(norm_val):
+                new_val = item * (max_val - min_val) + min_val
+                norm_val[idx] = new_val
+            return norm_val
+        else:
+            return norm_val * (max_val - min_val) + min_val 
 
-def main():
-    pass
+    def inference(self,value, normalize_method, model,minimum_price,maximum_price):
+        value = np.array(value)
+        predictions = []
+        for sample in value:
+            sample = np.array(sample).reshape(1,-1)
+            example = torch.tensor(normalize_method.transform(sample)).float()
+            
+            if(str(device) == 'cuda'):
+                example = example.to(device)
 
+            output = model(example)
+            output_unnorm = self.un_normalize(norm_val=output.detach(),min_val=minimum_price,max_val=maximum_price)
+            predictions.append(output_unnorm)
+        return predictions
+
+
+def main(normalizer,model,minimum_price,maximum_price):
+    inf = Inferencer()
+    print(minimum_price)
+    print(maximum_price)
+    output = inf.inference(value=[ [9400,9800,vol[-1]],
+                                   [9400,10000,35000]],
+                       normalize_method=normalizer,
+                       model=model,
+                       minimum_price=minimum_price,
+                       maximum_price=maximum_price
+                      )
+    print('BTC prediction: ', output)
+    
 if __name__ == '__main__':
-    main()
+    main(normalizer=min_max_scaler,model=price_model,minimum_price=min_price,maximum_price=max_price)
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch.utils.data
 """
 Basic multilayer perceptron subclass 
 of three layers in PyTorch
@@ -19,7 +19,7 @@ class MLP(nn.Module):
         x = F.leaky_relu(self.fc2(x))
         x = F.leaky_relu(self.fc3(x))
         return x
-      
+
 class NumberRegression_MLP(nn.Module):
     def __init__(self):
         super(NumberRegression_MLP,self).__init__()
@@ -32,37 +32,52 @@ class NumberRegression_MLP(nn.Module):
         x = F.leaky_relu(self.fc2(x))
         x = F.leaky_relu(self.fc3(x))
         return x
-
 """
 Recurrent Neural Networks (RNN)
 RNNs are an excellent method to model sequential data
 and time series data. This is perfect for stocks and
 crytocurrency price prediction.
+
+https://stackoverflow.com/questions/45022734/understanding-a-simple-lstm-pytorch
+https://www.jessicayung.com/lstms-for-time-series-in-pytorch/
 """
 class TestRNN(nn.Module):
-    def __init__(self,input_dim,hidden_dim,layer_amnt):
+    def __init__(self,bat_size,in_features,h_size,layer_amnt):
         super(TestRNN,self).__init__()
         
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
+        self.batch_sz = bat_size
+        self.in_features = in_features
+        self.h_size = h_size
         self.layer_amnt = layer_amnt
         
-        self.lstm1 = nn.LSTM(input_size=self.input_dim,
-                             hidden_size=self.hidden_dim,
-                             num_layers=1,
+        self.lstm1 = nn.LSTM(input_size=self.in_features,
+                             hidden_size=self.h_size,
+                             num_layers=self.layer_amnt,
                              bias=True,
-                             batch_first=False,
+                             batch_first=True,
                              dropout=0,
                              bidirectional=False
                             )
-        self.fc1 = nn.Linear(in_features=150,out_features=256)
-        self.fc2 = nn.Linear(in_features=256,out_features=128)
-        self.fc3 = nn.Linear(in_features=128,out_features=32)
-        self.fc4 = nn.Linear(in_features=32,out_features=1)
-        
-        pass
+    def init_hidden(self):
+        """Intialize/re-init the hidden and cell states. 
+        The hidden state acts as the memory of the RNN 
+        which gets passed from one unit to another. 
+        h_i = f(h_i + in)
+
+        Intializing with 0s
+        """
+        #print('layer size =\t', self.layer_amnt)
+        #print('bat_size =\t', self.batch_sz)
+        #print('hidden size =\t',self.h_size)
+        return (torch.zeros(self.layer_amnt,self.batch_sz,self.h_size),
+                torch.zeros(self.layer_amnt,self.batch_sz,self.h_size))
     def forward(self,x):
-        pass
+        x = x.unsqueeze(0)
+        hidden_init = self.init_hidden()
+        h0 = hidden_init[0].to(device)
+        c0 = hidden_init[1].to(device)
+        x,hidden = self.lstm1( x,(h0,c0))
+        return x
       
 """
 Temporal Convolutional Neural Network (CNN)
@@ -72,20 +87,57 @@ class TimeCNN(nn.Module):
     def __init__(self):
         super(TimeCNN,self).__init__()
         self.conv1 = nn.Conv1d(in_channels=1,out_channels=256,kernel_size=2,stride=1,padding=0)
-        self.mp1 = nn.MaxPool1d(kernel_size=2,stride=1)
-        self.conv2 = nn.Conv1d(in_channels=128,out_channels=256,kernel_size=3,stride=1,padding=0)
-        self.mp2 = nn.MaxPool1d(kernel_size=2,stride=1)
-        self.fc1 = nn.Linear(in_features=512,out_features=512)
-        self.fc2 = nn.Linear(in_features=512,out_features=128)
+        self.fc1 = nn.Linear(in_features=512,out_features=1024)
+        self.fc2 = nn.Linear(in_features=1024,out_features=128)
         self.fc3 = nn.Linear(in_features=128,out_features=1)
     def forward(self,x):
         x = x.unsqueeze(0)
         x = F.leaky_relu(self.conv1(x))
-        #x = self.mp1(x)
-        #x = F.relu(self.conv2(x))
-        #x = self.mp2(x)
         x = x.reshape(-1)
         x = F.leaky_relu(self.fc1(x))
         x = F.leaky_relu(self.fc2(x))
         x = F.leaky_relu(self.fc3(x))
+        return x
+
+"""
+https://arxiv.org/pdf/1506.00019.pdf
+https://www.quora.com/How-is-the-hidden-state-h-different-from-the-memory-c-in-an-LSTM-cell
+"""
+class TimeRNN(nn.Module):
+    def __init__(self,bat_size,in_features,h_size,layer_amnt):
+        super(TimeRNN,self).__init__()
+        
+        self.batch_sz = bat_size
+        self.in_features = in_features
+        self.h_size = h_size
+        self.layer_amnt = layer_amnt
+        
+        self.lstm1 = nn.LSTM(input_size=self.in_features,
+                             hidden_size=self.h_size,
+                             num_layers=self.layer_amnt,
+                             bias=True,
+                             batch_first=True,
+                             dropout=0,
+                             bidirectional=False)
+        self.fc1 = nn.Linear(in_features=1,out_features=1)
+    def init_hidden(self):
+        """Intialize/re-init the hidden and cell states. 
+        The hidden state acts as the memory of the RNN 
+        which gets passed from one unit to another. 
+        h_i = f(h_i + in)
+
+        Intializing with 0s
+        """
+        #print('layer size =\t', self.layer_amnt)
+        #print('bat_size =\t', self.batch_sz)
+        #print('hidden size =\t',self.h_size)
+        return (torch.zeros(self.layer_amnt,self.batch_sz,self.h_size),
+                torch.zeros(self.layer_amnt,self.batch_sz,self.h_size))
+    def forward(self,x):
+        x = x.unsqueeze(0)
+        hidden_init = self.init_hidden()
+        h0 = hidden_init[0].to(device)
+        c0 = hidden_init[1].to(device)
+        x,hidden = self.lstm1( x,(h0,c0))
+        x = F.leaky_relu(self.fc1(x[-1].view(self.batch_sz,-1)))
         return x
